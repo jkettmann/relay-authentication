bb651d15353cd4f704f33a4b5cb745caa82df5ee
Johannes Kettmann <jkettmann@gmx.net>
2017-06-13 15:22:07 +0200

    Extracted session middleware

--- server/graphQlServer.js
+++ server/graphQlServer.js
@@ -1,56 +1,18 @@
 import express from 'express'
 import graphQLHTTP from 'express-graphql'
-import cookieSession from 'cookie-session'
 
 import uploadMiddleWare from './uploadMiddleware'
-import { decodeToken } from './authentication'
+import sessionMiddleware from './sessionMiddleware'
 import Schema from './graphql/schema'
-
-function loadSessionData(req) {
-  if (req.session && req.session.token) {
-    return new Promise((resolve) => {
-      let tokenData = null
-      try {
-        tokenData = decodeToken(req.session.token)
-      } catch (err) {
-        // eslint-disable-next-line no-undef
-        log(err)
-      }
-      resolve(tokenData)
-    })
-  }
-
-  return new Promise((resolve) => {
-    resolve(null)
-  })
-}
-
-function getSessionData(req, res, next) {
-  loadSessionData(req)
-    .then((tokenData) => {
-      req.tokenData = tokenData || {}
-      next()
-    })
-    .catch(() => {
-      res.sendStatus(400)
-    })
-}
 
 export default function createGraphQlServer(port, database) {
   const graphQLServer = express()
 
-  graphQLServer.use(
-    cookieSession({
-      name: 'session',
-      keys: ['id', 'token'],
-    }),
-  )
-
+  graphQLServer.use(sessionMiddleware)
   graphQLServer.use('/graphql', uploadMiddleWare)
 
   graphQLServer.use(
     '/graphql',
-    getSessionData,
     graphQLHTTP(({ session, tokenData }) => ({
       graphiql: true,
       pretty: true,
---
+++ server/sessionMiddleware.js
@@ -0,0 +1,42 @@
+import cookieSession from 'cookie-session'
+
+import { decodeToken } from './authentication'
+
+function loadSessionData(req) {
+  if (req.session && req.session.token) {
+    return new Promise((resolve) => {
+      let tokenData = null
+      try {
+        tokenData = decodeToken(req.session.token)
+      } catch (err) {
+        // eslint-disable-next-line no-undef
+        log(err)
+      }
+      resolve(tokenData)
+    })
+  }
+
+  return new Promise((resolve) => {
+    resolve(null)
+  })
+}
+
+function getSessionData(req, res, next) {
+  loadSessionData(req)
+    .then((tokenData) => {
+      req.tokenData = tokenData || {}
+      next()
+    })
+    .catch(() => {
+      res.sendStatus(400)
+    })
+}
+
+const cookieMiddleware = cookieSession({
+  name: 'session',
+  keys: ['id', 'token'],
+})
+
+export default (req, res, next) => {
+  cookieMiddleware(req, res, () => getSessionData(req, res, next))
+}
